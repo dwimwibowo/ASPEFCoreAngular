c17b437069550665233e563eeac7b02a88ef8a16
Dwi Marstya Wibowo <dwimwibowo@gmail.com>
2021-12-04 17:09:11 +0700

    Enabling MVC

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -15,24 +15,29 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
-
-            app.UseDefaultFiles();
+            //app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseNodeModules(env);
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute(
+                    "Fallback",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", Action = "Index" }
+                );
+            });
         }
     }
 }
