c694e34ea90cadd5c276ad368d03fe81ac448be2
Dwi Marstya Wibowo <dwimwibowo@gmail.com>
2021-12-13 11:20:49 +0700

    Adding TypeScript

---
+++ DutchTreat/wwwroot/ts/main.js
@@ -0,0 +1,4 @@
+//import { StoreCustomer } from "./StoreCustomer";
+var shopper = new StoreCustomer("Dutch", "Treat");
+shopper.showName();
+//# sourceMappingURL=main.js.map
---
+++ DutchTreat/wwwroot/ts/main.ts
@@ -0,0 +1,4 @@
+﻿//import { StoreCustomer } from "./StoreCustomer";
+
+let shopper = new StoreCustomer("Dutch", "Treat");
+shopper.showName();
